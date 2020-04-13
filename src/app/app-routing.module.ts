e926a09c24e99934598b84fc85255240a2f6276e
max-student <max-student@maxtrain.com>
2020-04-13 17:26:49 -0400

    final commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './misc/home/home.component';
+import { AboutComponent } from './misc/about/about.component';
+import { ContactComponent } from './misc/contact/contact.component';
+import { HelpComponent } from './misc/help/help.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "/about", pathMatch: "full" }, // always the first route to put
+  { path: "home", component: HomeComponent }, // component is a class name
+  { path: "about", component: AboutComponent },
+  { path: "help", component: HelpComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "**", component: HelpComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
